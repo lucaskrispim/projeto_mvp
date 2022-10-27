fa2b25f736bc7eb210d6e96fbf9a5a1bcfc8a8bc
lucaskrispim <lucaskrispim@hotmail.com>
2022-10-27 00:16:50 -0300

    allowed hosts

--- projeto/settings.py
+++ projeto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n6*xgb^eqdaky)u2v4@(hn-76=))+$hjavh)20)%dx!2x_o^s5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
